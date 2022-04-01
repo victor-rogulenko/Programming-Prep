046c34a18869054b6025f08faf761c36ad6aba53
Victor Rogulenko <rogulenko@gmail.com>
2022-04-01 13:46:13 +0300

    Adjusted Template.cpp based on code from ksun48 in Code Jam 2021

--- CJ2021/Template.cpp
+++ CJ2021/Template.cpp
@@ -1,51 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// Created by Victor Rogulenko
+// Date and time: <press F5 to add>
 
+#include <bits/stdc++.h>
 using namespace std;
 
 //#define USE_STANDARD_IO true
 
-vector<vector<int>> ReadTestCases (int T) {
-    vector<vector<int>> result;
+void solve() {
 
-    for (int i = 0; i < T; i++) {
-        int N = 0;
-        cin >> N;
-        vector<int> current_case(N);
-        for (int j = 0; j < N; j++) {
-        	cin >> current_case[j];
-        }
-        result.push_back(current_case);
-    }
-
-    return result;
 }
-
-template<class ForwardIt>
-void PrintVector(ForwardIt first, ForwardIt last) {
-	for (auto it = first; it < last; it++) {
-		cout << *it << " ";
-	}
-	cout << endl;
-}
-
 
 int main() {
-    
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
     #ifndef USE_STANDARD_IO
 	    freopen("input.txt","r",stdin);
 	    freopen("output.txt","w",stdout);
 	#endif
-	
     int T = 0;
     cin >> T;
-
-    vector<vector<int>> test_cases = ReadTestCases(T);
-
-    for (int i = 0; i < T; i++) {
-    	/// solve
-    }
-
+    while (T--) solve();
 	return 0;
 }
