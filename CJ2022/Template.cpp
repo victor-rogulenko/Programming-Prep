5b4d98fc0dba9b0ccf512d61b097427833ea8895
Victor Rogulenko <rogulenko@gmail.com>
2022-04-01 20:32:23 +0300

    Moved Template.cpp ahead of CJ2022

---
+++ CJ2022/Template.cpp
@@ -0,0 +1,23 @@
+// Created by Victor Rogulenko
+// Date and time: <press F5 to add> 20:31 01/04/2022
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define USE_STANDARD_IO true
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    #ifndef USE_STANDARD_IO
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    int T = 0;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
