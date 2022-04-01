6af65f3139383288d1ba865cdbe8493d44341c42
Victor Rogulenko <rogulenko@gmail.com>
2022-04-01 13:46:54 +0300

    CJ2021 Median-Sort complete after review of ksun48's solution

--- CJ2021/Median-Sort.cpp
+++ CJ2021/Median-Sort.cpp
@@ -1,150 +1,70 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <exception>
-#include <string>
+// Created by Victor Rogulenko
+// Date and time: 12:35 01/04/2022
 
+#include <bits/stdc++.h>
 using namespace std;
 
-enum InquiryResult {LESS, GREATER, MEDIAN};
+int N, Q;
 
-
-InquiryResult MakeInquiry(int left, int right, int candidate) {
-
-    cout << left << " " << right << " " << candidate << endl;
-
-    InquiryResult result = MEDIAN;
-
-    int judge_reply;
-    cin >> judge_reply;
-
-    if (judge_reply == -1) {
-        throw exception();
-    } else if (judge_reply == left) {
-        result = LESS;
-        //cout << "LESS " << endl;
-    } else if (judge_reply == right) {
-        result = GREATER;
-        //cout << "GREATER " << endl;
-    } //else {
-      //  cout << "MEDIAN " << endl;
-    //}
-
-    return result;
+int Inquire(int left, int right, int candidate) {
+    cout << left+1 << " " << right+1 << " " << candidate+1 << endl;
+    int res;
+    cin >> res;
+    if (res == -1) exit(0);
+    return (res-1);
 }
 
 void PrintVector(const vector<int>& v) {
     for (auto it : v) {
-        cout << it << " ";
+        cout << it+1 << " ";
     }
     cout << endl;
 }
 
-void PlaceNewIndex(vector<int>& sorted, vector<int>::iterator left_border, 
-                   vector<int>::iterator right_border, int new_index) {
-    
-    int subvector_size = right_border - left_border;
-
-    // cout << "cheking " << *left_border << " and " << *right_border << endl;
-    // cout << "subvector_size = " << subvector_size << endl;
-    // PrintVector(sorted);
-
-    if (subvector_size < 0) {
-        sorted.insert(left_border, new_index);
-
-    } else if (subvector_size == 0) {
-        // we are pointing at one element
-        if (left_border != sorted.begin()) {
-            PlaceNewIndex(sorted, left_border-1, right_border, new_index);
-        } else {
-            PlaceNewIndex(sorted, left_border, right_border+1, new_index);
-        }
-
-    } else if (subvector_size < 4) {
-        // 2, 3 or 4 elements
-        InquiryResult inquiry_result = MakeInquiry(*left_border, *(left_border+1), new_index);
-        // cout << "flag1" << endl;
-        if (inquiry_result == LESS) {
-            // cout << "flag2" << endl;
-            sorted.insert(left_border, new_index);
-        } else if (inquiry_result == MEDIAN) {
-            // cout << "flag3" << endl;
-            sorted.insert(left_border+1, new_index);
-        } else {
-            // cout << "flag4" << endl;
-            if (subvector_size == 1) {
-                sorted.insert(left_border+2, new_index);
-            } else {
-                PlaceNewIndex(sorted, right_border-1, right_border, new_index);
-            }
-        }
-
-    } else if (subvector_size == 4) {
-        // 5 elements
-        InquiryResult inquiry_result = MakeInquiry(*(left_border+2), *(left_border+3), new_index);
-        if (inquiry_result == LESS) {
-            PlaceNewIndex(sorted, left_border, left_border+1, new_index);
-        } else if (inquiry_result == MEDIAN) {
-            sorted.insert(left_border+3, new_index);
-        } else {
-            PlaceNewIndex(sorted, right_border-1, right_border, new_index);
-        }
-
-    } else {
-        // 6 elements or more
-        vector<int>::iterator mid_left_border = sorted.begin() + (subvector_size+1)/3;
-        vector<int>::iterator mid_right_border = sorted.begin() + 2*(subvector_size+1)/3 -1;
-
-        InquiryResult inquiry_result = MakeInquiry(*(mid_left_border), *(mid_right_border), new_index);
-        if (inquiry_result == LESS) {
-            PlaceNewIndex(sorted, left_border, mid_left_border-1, new_index);
-        } else if (inquiry_result == MEDIAN) {
-            PlaceNewIndex(sorted, mid_left_border+1, mid_right_border-1, new_index);
-        } else {
-            PlaceNewIndex(sorted, mid_right_border+1, right_border, new_index);
-        }
-    }
-}
-
-void CheckJudgement() {
+void Judge() {
     int result;
     cin >> result;
-    if (result == 1) {
-        return;
-    } else {
-        throw exception();
-    }
+    if (result == -1) exit(0);
 }
 
-void Solve(int N) {
-    vector<int> sorted = {1, 2};
-
-    int new_index = 3;
-
-    while (new_index <= N) {
-        PlaceNewIndex(sorted, sorted.begin(), sorted.end()-1, new_index);
-        new_index++;
+void PlaceNewIndex(vector<int>& sorted, int j) {
+    int s = 0;
+    int e = (int)sorted.size() + 1;
+    while (s + 1 < e) {
+        int midleft = (2*s + e) / 3;
+        int midright = (s + 2*e) / 3;
+        if (midleft == 0) midleft++;
+        while (midright <= midleft) midright++;
+        if (midright == (int)sorted.size() + 1) midright--;
+        while (midleft >= midright) midleft--;
+        int res = Inquire(sorted[midleft-1], sorted[midright-1], j);
+        if (res == j) {
+            s = max(s, midleft);
+            e = min(e, midright);
+        } else if(res == sorted[midleft-1]) {
+            e = min(e, midleft);
+        } else {
+            s = max(s, midright);
+        }
     }
+    sorted.insert(sorted.begin() + s, j);
+}
 
+void Solve() {
+    vector<int> sorted = {0, 1};
+    for (int j = 2; j < N; j++) {
+        PlaceNewIndex(sorted, j);
+    }
     PrintVector(sorted);
-    CheckJudgement();
+    Judge();
 }
 
 int main() {
-	
-    int T = 0;
-    int N = 0;
-    int Q = 0;
-
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int T;
     cin >> T >> N >> Q;
-
     for (int i = 0; i < T; i++) {
-    	try {
-            Solve(N);
-        } catch (const exception& e) {
-            return(0);
-        }
+        Solve();
     }
-
 	return 0;
 }
