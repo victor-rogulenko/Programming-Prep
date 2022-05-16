061f0c5ff433d53b44a85e62f72cc4db54b0c499
Victor Rogulenko <rogulenko@gmail.com>
2022-05-16 17:31:19 +0300

    LeetCode: 144. Binary Tree Preorder Traversal -- Iterative

--- LeetCode/144-Binary-Tree-Preorder-Traversal.cpp
+++ LeetCode/144-Binary-Tree-Preorder-Traversal.cpp
@@ -22,6 +22,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+#include <vector>
+#include <set>
+#include <stack>
+
 class Solution {
     std::vector<int> result;
 public:
@@ -32,6 +37,40 @@ public:
         result.push_back(root->val);
         preorderTraversal(root->left);
         preorderTraversal(root->right);
+        return result;
+    }
+
+    std::vector<int> preorderTraversalIterative(TreeNode* root) {
+        if (!root) {
+            return result;
+        }
+        
+        std::set<TreeNode*> visited;
+        visited.insert(root);
+        
+        std::stack<TreeNode*> ancestry;
+        ancestry.push(root);
+        
+        result.push_back(root->val);
+        
+        while (!ancestry.empty()) {
+            if (ancestry.top()->left && 
+                visited.find(ancestry.top()->left) == visited.end()) {
+                
+                result.push_back(ancestry.top()->left->val);
+                visited.insert(ancestry.top()->left);
+                ancestry.push(ancestry.top()->left);
+            } else if (ancestry.top()->right && 
+                       visited.find(ancestry.top()->right) == visited.end()) {
+                
+                result.push_back(ancestry.top()->right->val);
+                visited.insert(ancestry.top()->right);
+                ancestry.push(ancestry.top()->right);
+            } else {
+                ancestry.pop();
+            }
+        }
+        
         return result;
     }
 };
