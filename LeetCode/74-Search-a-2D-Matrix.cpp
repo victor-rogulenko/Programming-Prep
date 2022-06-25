673e7eefa307c3f597bc686f0c0fedd66ef5a174
Victor Rogulenko <rogulenko@gmail.com>
2022-06-25 22:34:02 +0300

    LeetCode: 74. Search a 2D Matrix

--- LeetCode/74-Search-a-2D-Matrix.cpp
+++ LeetCode/74-Search-a-2D-Matrix.cpp
@@ -1,7 +1,24 @@
 // LeetCode: 74. Search a 2D Matrix
 // 74-Search-a-2D-Matrix.cpp
+// Created by Victor Rogulenko
+// [2022-06-25 22:16]	
 
+// Write an efficient algorithm that searches for a value target in an m x n 
+// integer matrix matrix. This matrix has the following properties:
+// - Integers in each row are sorted from left to right.
+// - The first integer of each row is greater than the last integer of 
+// the previous row.
 
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -10^4 <= matrix[i][j], target <= 10^4
+
+// MAJOR IDEA: 2 bin searches. Build an auxiliary vector of first elements
+// to use in the first bin search
+
+#include <vector>
+#include <algorightm> // std::upper_bound
 
 class Solution {
 public:
